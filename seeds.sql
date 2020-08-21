f60bcc863553220bac0de265f85dffee81f2c922
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-21 19:29:05 -0400

    added manager_id to employee

--- seeds.sql
+++ seeds.sql
@@ -17,8 +17,8 @@ INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 100, 4);
 
 ----- Employees Seeds -----
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Elizabeth", "Lee", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Johnson", "Pham", 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Sarah", "Abouelela", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Bobby", "Sporman", 5);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Andrea", "Gamez", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Elizabeth", "Lee", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Johnson", "Pham", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sarah", "Abouelela", 3, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Bobby", "Sporman", 5, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andrea", "Gamez", 1, 4);
